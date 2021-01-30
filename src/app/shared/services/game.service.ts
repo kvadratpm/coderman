3441a5f474b33f68b039dac2b5bbc9e2ab2deff7
Maksim Perevyazkin <kvadratpm@gmail.com>
2021-01-30 11:37:43 +0300

    feat: refactor project, delete old garbage

--- src/app/shared/services/game.service.ts
+++ src/app/shared/services/game.service.ts
@@ -14,15 +14,15 @@ export interface SceneConfig {
   /**
    * @param tileMap - параметры тайлмэпа текущего уровня
    */
-  tileMap: { // параметры тайлмэпа
+  tileMap: {
     /**
      * @param tileMap.key - ключ создаваемого уровня, используем именование level1, level2 и так далее
      */
-    key: string, // ключ для привязки
+    key: string,
     /**
      * @param tileMap.path - путь до файла, достаточно указать папку с уровнем и json файл
      */
-    path: string, // путь до файла
+    path: string,
   };
   /**
    * @param hero - параметры спрайта героя
@@ -32,15 +32,15 @@ export interface SceneConfig {
      * @param hero.key - ключ спрайта, именуем в соотвествии с используемым спрайтом,
      * например, используем спрайт hero1.png, ключ называем hero1
      */
-    key: string, // ключ героя
+    key: string,
     /**
      * @param hero.pngPath - имя png файла с героем
      */
-    pngPath: string, // путь до пнгшки героя
+    pngPath: string,
     /**
      * @param hero.jsonPath - имя json файла с героем
      */
-    jsonPath: string // путь до json героя
+    jsonPath: string
   };
 }
 
@@ -67,17 +67,6 @@ export class GameService extends Phaser.Scene {
   levelTarget = 0;
   isSuccess = false;
 
-  // временно
-  orc1!: any;
-  orc2!: any;
-  hero2!: any;
-  hero3!: any;
-  shield!: any;
-  head!: any;
-  sword!: any;
-  finish!: any;
-  // ********* */
-
   constructor(config: SceneConfig) {
     super({ key: 'main' });
     this.sceneConfig = config;
@@ -85,11 +74,18 @@ export class GameService extends Phaser.Scene {
 
   preload(): void {
     this.load.image('tiles', 'assets/map/tiles.png');
-    this.load.image('point', 'assets/phaser1/lighter1.png');
+
     this.load.tilemapTiledJSON(
       this.sceneConfig.tileMap.key,
-      `assets/${this.sceneConfig.tileMap.path}`
-    );
+      `assets/levels/${this.sceneConfig.tileMap.path}`
+    ); // тайл уровня
+
+    this.load.atlas(
+      this.sceneConfig.hero.key,
+      `assets/heroes/${this.sceneConfig.hero.pngPath}`,
+      `assets/heroes/${this.sceneConfig.hero.jsonPath}`
+    ); // тайл героя
+
     // *****MeNU*****
     this.load.image('button', 'assets/button/button.png');
     this.load.image('button_pressed', 'assets/button/button_pressed.png');
@@ -97,34 +93,23 @@ export class GameService extends Phaser.Scene {
     this.load.audio('backgroundMusic', 'assets/audio/second.mp3');
     // ****************************
 
-    this.load.atlas('gems', 'assets/phaser1/gems1.png', 'assets/phaser1/gems1.json');
-    this.load.image('tiles', 'assets/map/tiles.png'); // изображение с тайлами - оно одно везде?
-    this.load.image('point', 'assets/phaser1/lighter1.png');
-
-    this.load.tilemapTiledJSON(this.sceneConfig.tileMap.key,
-      `assets/${this.sceneConfig.tileMap.path}`); // тайлмэп текущего уровня
-    this.load.atlas(this.sceneConfig.hero.key,
-      `assets/heroes/${this.sceneConfig.hero.pngPath}`,
-      `assets/heroes/${this.sceneConfig.hero.jsonPath}`
-    );
-
-// **************временные текстуры***************
-    this.load.atlas('gems', 'assets/phaser1/gems1.png', 'assets/phaser1/gems1.json');
-    this.load.atlas('orc1', 'assets/enemies/orc1.png', 'assets/enemies/orc1.json'); // черт1
-    this.load.atlas('orc2', 'assets/enemies/orc2.png', 'assets/enemies/orc2.json'); // черт2
-    this.load.atlas('hero2', 'assets/heroes/hero1.png', 'assets/heroes/hero1.json'); // hero2
-    this.load.atlas('hero3', 'assets/heroes/warrior2.png', 'assets/heroes/warrior2.json'); // hero3
-
+    // ***Персонажи и текстуры***
+    this.load.atlas('orc1', 'assets/enemies/orc1.png', 'assets/enemies/orc1.json'); // орк1
+    this.load.atlas('orc2', 'assets/enemies/orc2.png', 'assets/enemies/orc2.json'); // орк2
+    this.load.atlas('hero3', 'assets/heroes/warrior2.png', 'assets/heroes/warrior2.json'); // странствующий рыцарь
     this.load.image('sword', 'assets/armour/sword.png'); // меч
     this.load.image('head', 'assets/armour/head.png'); // шлем
     this.load.image('shield', 'assets/armour/shield.png'); // щит
-    this.load.image('finish', 'assets/phaser1/finish.png'); // finish point
+    this.load.atlas('gems', 'assets/gems/gems1.png', 'assets/gems/gems1.json'); // камни
+    this.load.image('point', 'assets/points/lighter1.png'); // текущая точка
+    this.load.image('finish', 'assets/points/finish.png'); // финишная точка
+    // ********
   }
 
   create(): void {
     const map = this.make.tilemap({ key: 'map', tileWidth: this.cell, tileHeight: this.cell });
     const tileset = map.addTilesetImage('tiles', 'tiles');
-    const layer = map.createLayer(0, tileset, 0, 0);
+    const layer = map.createLayer('Ground', tileset, 0, 0); // id слоя по его названию в тайлсете
     layer.setScale(this.scaleCoef);
     const spawnPoint = map.findObject('Items', obj => obj.name === 'Spawn Point');
     const gemPoints = map.filterObjects('Items', elem => elem.name === 'Gem Point');
@@ -141,35 +126,38 @@ export class GameService extends Phaser.Scene {
       .setOffset(0, 24)
       .setScale(window.screen.width * 0.5 / 650);
     this.physics.add.collider(this.player, layer);
-    const anims = this.anims;
-    anims.create({
+
+    // ***Анимации героя***
+    this.anims.create({
       key: 'left',
-      frames: anims.generateFrameNames(this.sceneConfig.hero.key, { prefix: 'left.', start: 0, end: 3, zeroPad: 3 }),
+      frames: this.anims.generateFrameNames(
+        this.sceneConfig.hero.key,
+        { prefix: 'left.', start: 0, end: 3, zeroPad: 3 }
+        ),
       frameRate: 10,
       repeat: -1
     });
-    anims.create({
+    this.anims.create({
       key: 'right',
-      frames: anims.generateFrameNames(this.sceneConfig.hero.key, { prefix: 'right.', start: 0, end: 3, zeroPad: 3 }),
+      frames: this.anims.generateFrameNames(this.sceneConfig.hero.key, { prefix: 'right.', start: 0, end: 3, zeroPad: 3 }),
       frameRate: 10,
       repeat: -1
     });
-    anims.create({
+    this.anims.create({
       key: 'front',
-      frames: anims.generateFrameNames(this.sceneConfig.hero.key, { prefix: 'front.', start: 0, end: 3, zeroPad: 3 }),
+      frames: this.anims.generateFrameNames(this.sceneConfig.hero.key, { prefix: 'front.', start: 0, end: 3, zeroPad: 3 }),
       frameRate: 10,
       repeat: -1
-
     });
-    anims.create({
+    this.anims.create({
       key: 'back',
-      frames: anims.generateFrameNames(this.sceneConfig.hero.key, { prefix: 'back.', start: 0, end: 3, zeroPad: 3 }),
+      frames: this.anims.generateFrameNames(this.sceneConfig.hero.key, { prefix: 'back.', start: 0, end: 3, zeroPad: 3 }),
       frameRate: 10,
       repeat: -1
     });
+    // *************************
 
-
-// *********временные текстуры**********************************
+    // ***Анимации врагов ***/
     this.anims.create({
       key: 'stand1',
       frames: this.anims.generateFrameNames('orc1', { prefix: 'stand1.', start: 0, end: 13, zeroPad: 3 }),
@@ -188,73 +176,53 @@ export class GameService extends Phaser.Scene {
       frames: this.anims.generateFrameNames('orc2', { prefix: 'lay2.', start: 0, end: 14, zeroPad: 3 })
     });
     this.anims.create({
-      key: 'left2',
-      frames: anims.generateFrameNames('hero2', { prefix: 'left2.', start: 0, end: 3, zeroPad: 3 }),
-      frameRate: 10, repeat: -1
-    });
-    this.anims.create({
-      key: 'right2',
-      frames: anims.generateFrameNames('hero2', { prefix: 'right2.', start: 0, end: 3, zeroPad: 3 }),
-      frameRate: 10, repeat: -1
-    });
-    this.anims.create({
-      key: 'front2',
-      frames: anims.generateFrameNames('hero2', { prefix: 'front2.', start: 0, end: 3, zeroPad: 3 }),
-      frameRate: 10, repeat: -1
-    });
-    this.anims.create({
-      key: 'back2',
-      frames: anims.generateFrameNames('hero2', { prefix: 'back2.', start: 0, end: 3, zeroPad: 3 }),
-      frameRate: 10, repeat: -1
-    });
-    this.anims.create({
       key: 'wait',
-      frames: anims.generateFrameNames('hero3', { prefix: 'wait.', start: 0, end: 19, zeroPad: 3 }),
+      frames: this.anims.generateFrameNames('hero3', { prefix: 'wait.', start: 0, end: 19, zeroPad: 3 }),
       frameRate: 10, repeat: -1
     });
 
-    /*
-    this.orc1  = this.physics.add
-      .sprite(100 * this.scaleCoef, 100 * this.scaleCoef, 'orc1', 'stand1')
-      .setScale(window.screen.width * 0.5 / 650);
-    this.orc1.play('stand1');
-    this.orc2  = this.physics.add
-      .sprite(200 * this.scaleCoef, 100 * this.scaleCoef, 'orc2', 'stand2')
-      .setScale(window.screen.width * 0.5 / 650);
-    this.orc2.play('lay2');
-    this.hero2 = this.physics.add
-      .sprite(300 * this.scaleCoef, 100 * this.scaleCoef, 'hero2', 'front2')
-      .setScale(window.screen.width * 0.5 / 650);
-    this.hero3 = this.physics.add
-      .sprite(400 * this.scaleCoef, 100 * this.scaleCoef, 'hero3', 'wait')
-      .setScale(window.screen.width * 0.5 / 650);
-    this.hero3.play('wait');
-    this.sword = this.physics.add.image(500 * this.scaleCoef, 100 * this.scaleCoef, 'sword');
-    this.sword.setScale(this.scaleCoef);
-    this.head = this.physics.add.image(200 * this.scaleCoef, 200 * this.scaleCoef, 'head');
-    this.head.setScale(this.scaleCoef);
-    this.shield = this.physics.add.image(300 * this.scaleCoef, 200 * this.scaleCoef, 'shield');
-    this.shield.setScale(this.scaleCoef);
-    this.finish = this.physics.add.image(325 * this.scaleCoef, 325 * this.scaleCoef, 'finish');
-    this.finish.setScale(this.scaleCoef);
-    */
+    // *** Анимации камней ****/
+    this.anims.create({
+      key: 'diamond',
+      frames: this.anims.generateFrameNames(
+        'gems',
+        { prefix: 'diamond_', end: 15, zeroPad: 4 }
+      ),
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'prism',
+      frames: this.anims.generateFrameNames(
+        'gems',
+        { prefix: 'prism_', end: 6, zeroPad: 4 }
+      ),
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'ruby',
+      frames: this.anims.generateFrameNames(
+        'gems',
+        { prefix: 'ruby_', end: 6, zeroPad: 4 }
+      ),
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'square',
+      frames: this.anims.generateFrameNames(
+        'gems',
+        { prefix: 'square_', end: 14, zeroPad: 4 }
+      ),
+      repeat: -1
+    });
+    // ****************
 
-
-
-
-// *********временные **********************************
-
-
+/*
     const camera = this.cameras.main;
     camera.startFollow(this.player);
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    */
 
-    this.anims.create({ key: 'diamond', frames: this.anims.generateFrameNames('gems', { prefix: 'diamond_', end: 15, zeroPad: 4 }), repeat: -1 });
-    this.anims.create({ key: 'prism', frames: this.anims.generateFrameNames('gems', { prefix: 'prism_', end: 6, zeroPad: 4 }), repeat: -1 });
-    this.anims.create({ key: 'ruby', frames: this.anims.generateFrameNames('gems', { prefix: 'ruby_', end: 6, zeroPad: 4 }), repeat: -1 });
-    this.anims.create({ key: 'square', frames: this.anims.generateFrameNames('gems', { prefix: 'square_', end: 14, zeroPad: 4 }), repeat: -1 });
-
-    /// Лут, враги, звезды
+    // *** Рендер лута, врагов, камней ***
     if (gemPoints) {
       this.levelTarget += gemPoints.length;
       gemPoints.forEach((point) => {
@@ -296,13 +264,14 @@ export class GameService extends Phaser.Scene {
     }
   }
 
+  // **************
+
   update(): void {
     const distance = Phaser.Math.Distance.Between(this.player.x, this.player.y, this.SpawnX, this.SpawnY);
     if (this.player.body.speed > 0 && distance < 7) {
       this.player.body.reset(this.SpawnX, this.SpawnY);
       this.player.stop(true, null);
     }
-
   }
 
   async movePlayer(direction: number): Promise<void> {
@@ -375,7 +344,6 @@ export class GameService extends Phaser.Scene {
     }
     this.checkIfSuccess();
   }
-  // **************формула успешного окончания или нет  по умолчанию ФЕЙЛ*********************
 
   checkIfSuccess(): void {
     if (this.levelTarget === 0) {
@@ -393,7 +361,7 @@ export class SettingsMenu extends Phaser.Scene {
   constructor() {
     super({ key: 'settings' });
   }
-  create() {
+  create(): void {
     this.gameSettings = JSON.parse(localStorage.getItem('myGameSettings') || '{}');
     this.add.text(250, 40, 'Settings', {
       fontSize: '56px', color: '#ffffff'
@@ -407,13 +375,13 @@ export class SettingsMenu extends Phaser.Scene {
     const backButton = new Button(this, 180, 230, '#000', 'button', 'button_pressed', 'Back', 'navigation', 'back', 'main');
   }
 
-  playButtonSound() {
+  playButtonSound(): void {
     if (this.gameSettings[1].value) {
       this.sound.play('buttonSound');
     } else { this.sound.stopAll(); }
   }
 
-  toggleItem(button: { name: string; }, text: string) {
+  toggleItem(button: { name: string; }, text: string): void {
     if (button.name === 'sfx') {
       this.gameSettings[1].value = text === 'On' ? true : false;
     } else if (button.name === 'music') {
