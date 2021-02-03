e48f278f1bacd09d016c91888d72cb8c5baface4
pavel8293-pavel <pavel8293@gmail.com>
2021-02-03 13:47:34 +0200

    fix: fixed load, save

--- src/app/shared/services/button.service.ts
+++ src/app/shared/services/button.service.ts
@@ -1,12 +1,9 @@
 import * as Phaser from 'phaser';
-import {GameService} from 'src/app/shared/services/game.service'
 export class Button extends Phaser.GameObjects.Container {
   targetScene: any;
   currentText: any;
   savedGame!:any
   scene!: any;
-  config=JSON.parse(localStorage.getItem('myGameSettings') || '{}')
-  levelNum!:any
 
   constructor(
 
@@ -25,19 +22,6 @@ export class Button extends Phaser.GameObjects.Container {
     } else if (type === 'toggle') {
       this.currentText = text;
     }
-    this.levelNum = JSON.parse(localStorage.getItem('saved game') || '{}')
-    this.config[2].value.levelNumber = this.levelNum
-    this.config[2].value.tileMap.path = `level${this.levelNum}/level${this.levelNum}.json`
-    if(this.levelNum<=5){
-     this.config[2].value.hero.key = "emptyhero"
-     this.config[2].value.hero.pngPath = "emptyhero.png"
-     this.config[2].value.hero.jsonPath = "emptyhero.json"
-    }else{
-     this.config[2].value.hero.key = "hero1"
-     this.config[2].value.hero.pngPath = "hero1.png"
-     this.config[2].value.hero.jsonPath = "hero1.json"
-    }
-    console.log(this.config)
     const button = this.scene.add.image(x, y, key1).setInteractive();
     button.setScale(0.5 * window.screen.width * 0.5 / 650);
     const buttonText = this.scene.add.text(x, y, text, {
@@ -51,20 +35,6 @@ export class Button extends Phaser.GameObjects.Container {
       button.setTexture(key2);
       this.scene.playButtonSound();
     });
-    button.on('pointerup', ()=>{
-      if (type === 'save') {
-        localStorage.setItem('saved game', JSON.stringify(JSON.parse(localStorage.getItem('currentLevel') || '{}')))
-      }else if (type === 'load') {
-        console.log(this.config)
-
-        setTimeout(() => {
-          this.scene.scene.stop(this.scene);
-          const load = new GameService (this.config)
-        }, 300);
-
-      }
-
-    })
     button.on('pointerup', () => {
       button.setTexture(key1);
       if (this.targetScene) {
--- src/app/shared/services/menu.service.ts
+++ src/app/shared/services/menu.service.ts
@@ -43,3 +43,4 @@ export class SettingsMenu extends Phaser.Scene {
   }
 }
 
+
